4d9289512f30ccce21ec01f383121092be89a7f2
suhrud-caizin <suhrud@caizin.com>
2025-12-19 17:20:54 +0530

    adds graph traversal

--- src/Graph/traversal/Traversal.java
+++ src/Graph/traversal/Traversal.java
@@ -46,4 +46,87 @@ public class Traversal {
 
         return ans;
     }
+
+
+    public record Pair(int node, int parent) {}
+
+    public boolean isCycle(int n, List<List<Integer>> adj){
+        boolean[] visited = new boolean[n+1];
+
+        for(int i = 1; i <= n; i++){
+            if(!visited[i]){
+                if(detectCycleBFS(i, adj, visited)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean detectCycleBFS(int src, List<List<Integer>> adj, boolean[] visited) {
+        Queue<Pair> queue = new LinkedList<>();
+        visited[src] = true;
+        queue.add(new Pair(src, -1));
+
+        while (!queue.isEmpty()){
+            var elem = queue.poll();
+            for(var neighbour : adj.get(elem.node - 1)){
+                if(!visited[neighbour]){
+                    visited[neighbour] = true;
+                    queue.add(new Pair(neighbour, elem.node));
+                } else if (neighbour != elem.parent) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean detectCycleDFS(int src, int parent, List<List<Integer>> adj, boolean[] visited) {
+        visited[src] = true;
+        for(int neighbour : adj.get(src-1)){
+            if(!visited[neighbour]){
+                if(detectCycleDFS(neighbour, src, adj, visited)) return true;
+            }else if(neighbour != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isCyclicInDirectedGraph(int n, List<List<Integer>> adj) {
+        int[] visited = new int[n+1];
+        int[] pathVisited = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            if(visited[i] == 0){
+                if(detectCycleInDirectedDFS(i, adj, visited, pathVisited)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean detectCycleInDirectedDFS(int src, List<List<Integer>> adj, int[] visited, int[] pathVisited) {
+
+        visited[src] = 1;
+        pathVisited[src] = 1;
+
+        for (int neighbour : adj.get(src-1)){
+            if(visited[neighbour] == 0){
+                if(detectCycleInDirectedDFS(neighbour, adj, visited, pathVisited)){
+                    return true;
+                }
+                pathVisited[neighbour] = 0;
+            }else if(visited[neighbour] == 1) return true;
+        }
+
+        pathVisited[src] = 0;
+
+        return false;
+    }
 }
